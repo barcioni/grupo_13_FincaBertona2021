7776d7b6e3637d65720d97270543ff7a7a684a39
rosariobertona <rosariobertona96@gmail.com>
2021-06-23 11:53:51 -0300

    Rutas editar y alta listas

--- web_finca_bertona/src/controllers/tiendaController.js
+++ web_finca_bertona/src/controllers/tiendaController.js
@@ -4,6 +4,12 @@ const controlador = {
     tienda: (req,res)=>{
         res.render(path.resolve(__dirname,"../views","products", "tienda.ejs"))
     },
+    alta: (req,res)=>{
+        res.render(path.resolve(__dirname,"../views","products", "alta.ejs"))
+    },
+    edicion: (req,res)=>{
+        res.render(path.resolve(__dirname,"../views","products", "edicion.ejs"))
+    },
 
 };
 
--- web_finca_bertona/src/routes/tienda.js
+++ web_finca_bertona/src/routes/tienda.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const tiendaController = require ("../controllers/tiendaController");
 
 router.get("/", tiendaController.tienda)
-/*router.get("/alta", tiendaController.alta)
-router.get("/edicion", tiendaController.edicion)*/
+router.get("/alta", tiendaController.alta)
+router.get("/edicion", tiendaController.edicion)
 module.exports = router;
