cf99fe056d3c76cc3f3aa5537ee9133de2657c07
rosariobertona <rosariobertona96@gmail.com>
2021-06-30 16:19:39 -0300

    arreglo

--- web_finca_bertona/src/controllers/tiendaController.js
+++ web_finca_bertona/src/controllers/tiendaController.js
@@ -12,16 +12,11 @@ const controlador = {
     edicion: (req,res)=>{
         res.render(path.resolve(__dirname,"../views","products", "edicion.ejs"))
     },
-<<<<<<< HEAD
     detalle: (req,res)=>{
         res.render(path.resolve(__dirname,"../views","products", "detalle.ejs"), {product:product.one(req.params.id)})},
 
 };
  
 
-
-=======
-};
->>>>>>> 6abf3edd4fb1939465fb283cf0b0ca9c08dda7fb
 
 module.exports = controlador ;
