02980d84e5db910137f31999429088339d3e69c5
barcioni <barcioni@gmail.com>
2021-07-20 17:15:10 -0300

    pruebas exp-val

--- web_finca_bertona/src/controllers/userController.js
+++ web_finca_bertona/src/controllers/userController.js
@@ -26,14 +26,14 @@ const controlador = {
     guardar: (req,res) => {
         //return res.send ({data:req.body, file:req.file})
         const resultValidation = validationResult(req);
-        store: (req,res) => {
+       /* store: (req,res) => {
             let errores = validationResult(req);
             if (!errores.isEmpty()){
                 return res.render ("registro", {mensadeDeError: errores.mapped()})
             }else{
                     res.render('users/login', {errors: errors.array()});
             }
-        }
+        }*/
         if (resultValidation.errors.length > 0) {
             return res.render('users/registro.ejs', {
                 errors: resultValidation.mapped(),
