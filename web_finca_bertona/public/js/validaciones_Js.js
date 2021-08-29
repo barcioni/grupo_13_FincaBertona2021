aea976e828bfa9b5c416a6b1b05c4993c4008d89
barcioni <barcioni@gmail.com>
2021-08-29 12:22:46 -0300

    comienzo validadciones del front

---
+++ web_finca_bertona/public/js/validaciones_Js.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function(){
+let formValidation = document.querySelector("");
+
+formValidation.addEventListener("submit", function(e){
+e.preventDefault();
+
+
+});
+
+
+
+});
