46c64d456b133b18ce4374ed6351a2e7f5979889
barcioni <barcioni@gmail.com>
2021-09-14 17:44:58 -0300

    updates validaciones

--- web_finca_bertona/public/js/validacionesAlta.js
+++ web_finca_bertona/public/js/validacionesAlta.js
@@ -1,12 +1,21 @@
 window.addEventListener("load", function(){
-    let formValidation = document.querySelector("form.control");
-    
+    let formValidation = document.querySelector("form.formulario-alta-productos");
+let errores = [];
+let altaProductos = document.querySelector("input.alta");
+if(altaProductos.value == ""){
+errores.push("El campo nombre no debe quedar vacío");
+}
+
+if(errores.length > 0){
+    event.preventDefault();
+    let ulErrores = document.querySelector(".errores ul");
+    errores.forEach(error => {
+    ulErrores.innerHTML += '<li>${error}</li>'
+    });
+    }
+    /*
     formValidation.addEventListener("submit", function(e){
     e.preventDefault();
-    
-    
     });
-    
-    
-    
+    */
     });
