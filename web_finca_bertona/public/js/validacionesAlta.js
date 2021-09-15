6f777805c04935c0f1cb3a7b4917951b76a9eb17
barcioni <barcioni@gmail.com>
2021-09-14 21:33:33 -0300

    updates validaciones front

--- web_finca_bertona/public/js/validacionesAlta.js
+++ web_finca_bertona/public/js/validacionesAlta.js
@@ -2,11 +2,18 @@ window.addEventListener("load", function(){
 let alta = document.querySelectorAll(".alta");
 let botonEnviar =  document.querySelector(".boton-formulario-enviar");
 
-botonEnviar.addEventListener("click", function(){
-    
+botonEnviar.addEventListener("click", function(e){
+e.preventDefault();
 })
-
-
+let errores = [];
+if(alta.value.length < 2){
+errores.name = "Este campo debe estar completo";
+}
+if(errores.length > 0){
+alta.innerText = (errores.name) ? errores.name : "";
+}else{
+    alta.submit();
+}
     });
 
 
