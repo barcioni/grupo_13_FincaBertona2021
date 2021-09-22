d4f8181660bf5895957cf9184c434fd93033c0eb
barcioni <barcioni@gmail.com>
2021-09-22 16:57:46 -0300

    updates dashboard

--- dashboard-finca-bertona/src/App.js
+++ dashboard-finca-bertona/src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>probando dashboard 1 2 3</p>
+        <h1>FINCA BERTONA</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
