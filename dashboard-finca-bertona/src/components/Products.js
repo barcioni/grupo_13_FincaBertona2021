dc95f09a674067aa47542cdbb14524457c8b90d6
rosariobertona <rosariobertona96@gmail.com>
2021-09-22 15:25:15 -0300

    Comienzo de React

---
+++ dashboard-finca-bertona/src/components/Products.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useEffect, useState} from "react";
+
+function Products() {
+
+    const [ products, setProducts] = useState ([ ])
+	useEffect(() => {
+		console.log("%cse creó el componente", "color: green");
+		fetch ("http://localhost:3030/api/products")
+		.then (response => response.json ())
+		.then (data => {setProducts (data)})
+		
+		.catch ( error => console.error (error))
+	}, [ ]);
+	
+	useEffect(() => {
+		console.log("%cactualización" , "color:yellow");
+	}, [products]);
+
+
+    return (
+	<div>
+    <h1>ACÁ VAN LA TARJETA DE PRODUCTOS</h1>
+	<div>
+	{
+		products.length > 0 && products.map((product, i) => {
+			return (
+				<div className="col-sm-6 col-md-3 my-4" key={i}>
+					<h5>{products.total}</h5>
+					<p>{product.year}</p>
+				</div>
+			)
+		})
+	}
+	</div>
+	</div>
+	
+	)
+
+    
+}
+
+
+export default Products;
