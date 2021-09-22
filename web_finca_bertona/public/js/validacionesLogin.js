7625e795a1873df6f926d85438fce2a216ff305d
barcioni <barcioni@gmail.com>
2021-09-22 18:25:41 -0300

    updates

--- web_finca_bertona/public/js/validacionesLogin.js
+++ web_finca_bertona/public/js/validacionesLogin.js
@@ -6,15 +6,24 @@ e.preventDefault();
 let campoNombre = document.querySelector("input.control");
 let campoMail = document.querySelector("input#email");
 let campoPassword = document.querySelector("input#clave");
-
+let errores = [];
+let contenedorErrores = document.querySelector(".errores")
 if (campoNombre.value == ""){
-    console.log('Este campo no puede quedar vacío')
+    errores.push({etiqueta: campoNombre, msg:'Este campo no puede quedar vacío'})
 }
 if (campoMail.value == ""){
-    console.log('Este campo no puede quedar vacío')
+    errores.push({etiqueta: campoMail, msg:'Este campo no puede quedar vacío'})
 }
 if (campoPassword.value == ""){
-    console.log('Este campo no puede quedar vacío')
+    errores.push({etiqueta: campoPassword, msg:'Este campo no puede quedar vacío'})
 }
+if(errores.length == 0){
+    e.target.submit(); 
+}else{
+    contenedorErrores.innerHTML = null;
+    errores.forEach(error =>{
+        contenedorErrores.innerHTML += `<small class="textoErrores">${error.msg}</small>`;
+    })
+} 
 });
 });
